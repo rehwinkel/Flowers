8b4e10d792d06c4ce2519c243ba128bac3ffa4cc
deerangle2 <ian.rehwinkel@kabelmail.de>
2018-07-21 10:42:46 +0200

    generated 100 images

--- auto-encoder.py
+++ auto-encoder.py
@@ -19,7 +19,7 @@ def saveVectorAsImage(id, vector):
 	pixels = [0] * 4096
 	for v in range(len(vector)):
 		#print(vector[v])
-		pixels[v] = int(abs(vector[v]) * 255)
+		pixels[v] = int(min(max(0, vector[v]), 1) * 255)
 	pixels = np.array(pixels).reshape(64, 64)
 	Image.fromarray(pixels.astype("uint8"), "L").save("gen/gen" + str(id) + ".jpg", "JPEG")
 
@@ -41,8 +41,8 @@ class NeuralNetwork:
 
 		self.logits = tf.nn.leaky_relu(tf.matmul(prev_layer_out, weights_last) + biases_last)
 
-		self.cost_fun = 0.000000006 * tf.reduce_sum(tf.subtract(self.logits, self.outputs) * tf.subtract(self.logits, self.outputs))
-		self.train_step = tf.train.GradientDescentOptimizer(0.000002).minimize(self.cost_fun)
+		self.cost_fun = 0.000005 * tf.reduce_sum(tf.subtract(self.logits, self.outputs) * tf.subtract(self.logits, self.outputs))
+		self.train_step = tf.train.GradientDescentOptimizer(0.005).minimize(self.cost_fun)
 		self.session.run(tf.global_variables_initializer())
 
 	def train(self, in_data, out_data, iterations):
@@ -74,8 +74,11 @@ image_size = 4096
 training_inputs = images.copy()#[[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]]
 training_outputs = images.copy()#[[0.0], [1.0], [1.0], [0.0]]
 
-my_network = NeuralNetwork([image_size, 1024, 128, 1024, image_size])
-my_network.train(training_inputs, training_outputs, 100)
+my_network = NeuralNetwork([image_size, 128, image_size])
+for i in range(100):
+	my_network.train(training_inputs, training_outputs, 100)
+	print(100 + 100 * i, "iterations")
+	saveVectorAsImage(i, my_network.feed(images[0]).flatten())
 
-saveVectorAsImage(0, my_network.feed(images[0]).flatten())
-saveVectorAsImage(1, images[0].flatten())
+#saveVectorAsImage(0, my_network.feed(images[0]).flatten())
+#saveVectorAsImage(1, images[0].flatten())
