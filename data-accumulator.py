b027babaa473bd7d603c07dbfd3d81332e3f8c15
deerangle2 <ian.rehwinkel@kabelmail.de>
2018-07-23 15:55:33 +0200

    dataset

--- data-accumulator.py
+++ data-accumulator.py
@@ -3,12 +3,16 @@ import numpy as np
 import imageio
 filename = "../videoplayback.mp4"
 video = imageio.get_reader(filename, "ffmpeg")
+
+count = 0
 for i, im in enumerate(video):
-	if i == 20000:
+	if count == 20000:
 		break
-	print(i)
-	img = Image.fromarray(np.uint8(im))
-	off_x = 0.5 * (img.width - img.height)
-	cropped = img.crop((off_x, 0, img.height + off_x, img.height))
-	cropped.thumbnail((128, 128))
-	cropped.convert('L').save("train/img" + str(i) + ".jpg")
+	if i % 10 == 0:
+		count += 1
+		print(count)
+		img = Image.fromarray(np.uint8(im))
+		off_x = 0.5 * (img.width - img.height)
+		cropped = img.crop((off_x, 0, img.height + off_x, img.height))
+		cropped.thumbnail((128, 128))
+		cropped.convert('L').save("train/img" + str(count) + ".jpg")
